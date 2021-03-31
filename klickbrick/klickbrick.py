beac6f1d501c3fbe3520bf39952143d74604a54e
Vinicios Wentz <wentz.vinicios@gmail.com>
2021-03-31 16:28:13 -0300

    feature: now cli parses --name argument

--- klickbrick/klickbrick.py
+++ klickbrick/klickbrick.py
@@ -1,12 +1,16 @@
 from argparse import ArgumentParser
 import sys
 
+
 def main() -> None:
     parser = ArgumentParser(description="Klickbrick CLI")
     parser.add_argument("hello", metavar="hello", type=str, default="")
+    parser.add_argument("--name", "-n", metavar="name",
+                        type=str, default="world")
     args = parser.parse_args()
 
-    print('Helo World')
+    print(f'{args.hello} {args.name}')
+
 
 if __name__ == "__main__":
     main()

925b8e14c9edac9b5dc9432a9f1099197a90576f
Vinicios Wentz <wentz.vinicios@gmail.com>
2021-04-05 11:14:05 -0300

    refactor add typing

--- clickbrick_cli/hello.py
+++ clickbrick_cli/hello.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from typing import Optional, Sequence
 import sys
 
 
@@ -7,13 +8,13 @@ def main() -> None:
     print(string)
 
 
-def process_arguments(args) -> str:
+def process_arguments(argv: Optional[Sequence[str]] = None) -> str:
     parser = ArgumentParser(description="Klickbrick CLI")
     parser.add_argument("hello", metavar="hello", type=str, default="")
     parser.add_argument("--name", "-n", metavar="name",
                         type=str, default="world")
-    args = parser.parse_args(args)
-
+    args = parser.parse_args(argv)
+    
     return f'{args.hello} {args.name}'
 
 
--- tests/test_klickbrick.py
+++ tests/test_klickbrick.py
@@ -4,6 +4,6 @@ from clickbrick_cli import hello
 
 
 class Klickbrick(TestCase):
-    def test_get_hello_name(self):
+    def test_get_hello_name(self) -> None:
         string = hello.process_arguments(["hello", "--name", "name"])
         self.assertEqual(string, "hello name")
